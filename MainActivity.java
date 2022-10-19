ab7ee41022cece7598982b0ff94950b1a0c93c7b
AleccDonald <89406414+AleccDonald@users.noreply.github.com>
2022-10-19 17:46:32 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,30 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return true;
-    }
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.item1:
-                Intent myintent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(myintent);
-                return false;
-            case R.id.item2:
-                Toast.makeText(this, "Item 2 is selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(this, "Item 3 is selected", Toast.LENGTH_SHORT).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+        });
+    }
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        startActivity(intent);
+
     }
 }
--- MainActivity2.java
+++ MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFirstActivity();
+            }
+
+        });
+    }
+    private void goToFirstActivity() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
     }
 }
